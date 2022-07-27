48b2a542691ad90d227c47e64913a310194ffddc
ichibanbrew <mikhail.nemtsev@gmail.com>
2022-07-27 15:00:00 +0700

    Update browser script

--- browser.js
+++ browser.js
@@ -1,4 +1,24 @@
-// Helper script for forum migration.
 (() => {
-    console.log("Version 1.0");
+    if (!window.location.pathname.includes("viewtopic.php")) { return; }
+    
+    let data = sessionStorage.getItem("posts");
+    if (!data) { return; }
+
+    let posts = JSON.parse(data);
+    if (!posts.length) { return; }
+    
+    console.log("# of Posts: " + posts.length);
+
+    let textarea = document.getElementsByTagName("textarea")[0];
+    if (!textarea) { 
+        alert("No textarea for reply");
+        return; 
+    }
+
+    textarea.value = posts.shift();
+    sessionStorage.setItem("posts", JSON.stringify(posts)); 
+
+    setTimeout(() => {
+        document.getElementsByName("post")[0].click();
+    }, 3500);
 })();
